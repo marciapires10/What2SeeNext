214c8fe36055c7cb61d63d2b05ab459ed8a0733f
tomasfilipe7 <tomasfilipe7@ua.pt>
2020-11-17 00:01:23 +0000

    update news feed rss

--- webapp/views.py
+++ webapp/views.py
@@ -12,14 +12,37 @@ MOVIES_NEWS = "https://www.cinemablend.com/rss/topic/news/movies"
 
 
 def get_rss(request):
+    print("OI")
     response = requests.get(MOVIES_NEWS)
     tree = etree.XML(response.text)
-    titles = ""
-    for item in tree.find("channel"):
+    movies = []
+    titles = []
+    resume = []
+    urls = []
+    items = tree.xpath(".//item")
+    for item in items:
         if item.find("title") is not None and item.find("description") is not None:
-            titles += "\n-> Title: " + item.find("title").text + "\n" + item.find("description").text + "\n "
 
-    return HttpResponse(titles)
+            titles.append(item.find("title").text)
+            description = item.find("description").text.replace("<p>","")
+            description = description.replace("</p>","")
+            description = description.replace("<em>","")
+            description = description.replace("</em>", "")
+            resume.append(description)
+
+            movie = []
+            movie.append(item.find("title").text)
+            movie.append(description)
+
+            if item.find("enclosure") is not None:
+                urls.append(str(item.find("enclosure").get('url')))
+                print(str(item.find("enclosure").get('url')))
+                movie.append(str(item.find("enclosure").get('url')))
+            movies.append(movie)
+
+    t_params = {'movies': movies,}
+
+    return render(request, 'index.html', t_params)
 
 
 def index(request):
